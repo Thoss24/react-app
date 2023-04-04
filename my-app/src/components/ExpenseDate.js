dce7c245b358e03fcadd595f4b17417b1f1805be
Thomas Fogarty <t.fogarty24@outlook.com>
2023-04-04 20:26:32 +0100

    splitting componenets into multiple components

---
+++ my-app/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(prop) {
+    const day = prop.date.toLocaleString("en-UK", { day: "2-digit"});
+    const month = prop.date.toLocaleString("en-UK", { month: "long"});
+    const year = prop.date.getFullYear();
+
+    return (
+        <div className="expense-date">
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+      </div>
+    )
+}
+
+export default ExpenseDate
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(prop) { // only one root element allowed per statement/JSX code snippet
-  const day = prop.date.toLocaleString("en-UK", { day: "2-digit"});
-  const month = prop.date.toLocaleString("en-UK", { month: "long"});
-  const year = prop.date.getFullYear();
  
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={prop.date}/>
       <div className="expense-item__description">
         <h2>{prop.title}</h2>
       </div>

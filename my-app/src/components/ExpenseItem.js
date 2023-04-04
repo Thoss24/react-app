2a2bd45d3e8937b68721f089400369e6958259c8
Thomas Fogarty <t.fogarty24@outlook.com>
2023-04-04 19:46:16 +0100

    logic to display day month year added

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(prop) { // only one root element allowed per statement/JSX code snippet
+  const day = prop.date.toLocaleString("en-UK", { day: "2-digit"});
+  const month = prop.date.toLocaleString("en-UK", { month: "long"});
+  const year = prop.date.getFullYear();
  
   return (
     <div className="expense-item">
-      <div>{prop.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{prop.title}</h2>
       </div>

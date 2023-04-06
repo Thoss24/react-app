3c2f45d58985b2bedd3f4cdd533bce46293168d3
Thomas Fogarty <t.fogarty24@outlook.com>
2023-04-06 19:17:04 +0100

    Card component added

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css'
 
 function ExpenseItem(prop) { // only one root element allowed per statement/JSX code snippet
  
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={prop.date}/>
       <div className="expense-item__description">
         <h2>{prop.title}</h2>
       </div>
       <div className="expense-item__price">{prop.amount}</div>
-    </div>
+    </Card>
   );
 }
 

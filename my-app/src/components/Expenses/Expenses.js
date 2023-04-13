fffaf2009b78c942970f73c277e7c9880a96e76c
Thomas Fogarty <t.fogarty24@outlook.com>
2023-04-13 20:37:45 +0100

    rendering lists of data dynamically

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -2,50 +2,41 @@ import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
-import './Expenses.css'
+import "./Expenses.css";
 
 const Expense = (props) => {
-
-  const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState("2023");
 
   const filterByYear = (year) => {
-    console.log(year)
-    console.log("Yes, it's working!")
+    console.log(year);
+    console.log("Yes, it's working!");
 
-    let expenseItemsYears = props.items.map((x) => {return x['date'].getFullYear() }); // Variable which holds the year for each expense item
+    let expenseItemsYears = props.items.map((x) => {
+      return x["date"].getFullYear();
+    }); // Variable which holds the year for each expense item
 
-    console.log(expenseItemsYears)
+    console.log(expenseItemsYears);
 
-    setFilteredYear(year)
+    setFilteredYear(year);
   };
 
   return (
     <div>
-    <Card className="expenses">
-    <ExpenseFilter defaultYear={filteredYear} expenseFilterYear={filterByYear}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        date={props.items[0].date}
-        amount={props.items[0].amount}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        date={props.items[1].date}
-        amount={props.items[1].amount}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        date={props.items[2].date}
-        amount={props.items[2].amount}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        date={props.items[3].date}
-        amount={props.items[3].amount}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpenseFilter
+          defaultYear={filteredYear}
+          expenseFilterYear={filterByYear}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
+      </Card>
     </div>
   );
-}
+};
 
 export default Expense;
